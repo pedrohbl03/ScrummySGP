e4d035ec1e638c1c71a672816ebdd375ac5be821
Pedro Lima <pedrozin1234@gmail.com>
2021-05-29 10:52:27 -0300

    Example final leading page project

--- js/custom.js
+++ js/custom.js
@@ -453,4 +453,16 @@
     });
 
 
+    $('.navbar-nav a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+                targetOffset = $(id).offset().top;
+                
+        $('html, body').animate({ 
+            scrollTop: targetOffset - 80
+        }, 500);
+    });
+
 }(jQuery));
+
+
